dab27594ea6118fda497b0b41989033b03426144
Daniel Semeniuta <dsemeniu@stanford.edu>
2019-12-05 01:34:25 -0800

    uncomment

--- models/cdcgan/train.py
+++ models/cdcgan/train.py
@@ -18,46 +18,46 @@ from tqdm import tqdm
 # from model import Generator, Discriminator
 from models.cdcgan.model import Generator, Discriminator
 
-# def get_default_args():
-#     parser = argparse.ArgumentParser('CDCGAN Training')
-#     parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
-#     parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
-#     parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
-#     parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
-#     parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
-#     parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
-#     parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
-#     parser.add_argument("--n_classes", type=int, default=10, help="number of classes for dataset")
-#     parser.add_argument("--img_size", type=int, default=32, help="size of each image dimension")
-#     parser.add_argument("--channels", type=int, default=1, help="number of image channels")
-#     parser.add_argument("--sample_interval", type=int, default=400, help="interval between image sampling")
-#     parser.add_argument("--save_every", type=int, default=25, help="interval between saving the model")
-#     parser.add_argument("--checkpoint_dir", type=str, default='./checkpoints', help="Checkpoint directory")
-#     parser.add_argument("--run_name", required=True, type=str)
-#     parser.add_argument("--use_cuda", action='store_true', help="Use CUDA if available")
-#     parser.add_argument("--load_checkpoint", action='store_true', help="Run from checkpoint")
-#     args = parser.parse_args()
-#     if args.use_cuda:
-#         args.use_cuda = torch.cuda.is_available()
-#         print('using cuda') if args.use_cuda else print('not using cuda')
-#     return args
-#
-# def get_mnist_dataloader(args):
-#     # Configure data loader
-#     os.makedirs("../../data/mnist", exist_ok=True)
-#     dataloader = torch.utils.data.DataLoader(
-#         datasets.MNIST(
-#             "../../data/mnist",
-#             train=True,
-#             download=True,
-#             transform=transforms.Compose(
-#                 [transforms.Resize(args.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
-#             ),
-#         ),
-#         batch_size=args.batch_size,
-#         shuffle=True,
-#     )
-#     return dataloader
+def get_default_args():
+    parser = argparse.ArgumentParser('CDCGAN Training')
+    parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
+    parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
+    parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
+    parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
+    parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
+    parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
+    parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
+    parser.add_argument("--n_classes", type=int, default=10, help="number of classes for dataset")
+    parser.add_argument("--img_size", type=int, default=32, help="size of each image dimension")
+    parser.add_argument("--channels", type=int, default=1, help="number of image channels")
+    parser.add_argument("--sample_interval", type=int, default=400, help="interval between image sampling")
+    parser.add_argument("--save_every", type=int, default=25, help="interval between saving the model")
+    parser.add_argument("--checkpoint_dir", type=str, default='./checkpoints', help="Checkpoint directory")
+    parser.add_argument("--run_name", required=True, type=str)
+    parser.add_argument("--use_cuda", action='store_true', help="Use CUDA if available")
+    parser.add_argument("--load_checkpoint", action='store_true', help="Run from checkpoint")
+    args = parser.parse_args()
+    if args.use_cuda:
+        args.use_cuda = torch.cuda.is_available()
+        print('using cuda') if args.use_cuda else print('not using cuda')
+    return args
+
+def get_mnist_dataloader(args):
+    # Configure data loader
+    os.makedirs("../../data/mnist", exist_ok=True)
+    dataloader = torch.utils.data.DataLoader(
+        datasets.MNIST(
+            "../../data/mnist",
+            train=True,
+            download=True,
+            transform=transforms.Compose(
+                [transforms.Resize(args.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
+            ),
+        ),
+        batch_size=args.batch_size,
+        shuffle=True,
+    )
+    return dataloader
 
 def sample_image(generator, *, n_row, batches_done, args):
     """Saves a grid of generated digits ranging from 0 to n_classes"""
