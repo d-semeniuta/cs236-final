beaff7178de04a5e08d25f3f7bb71e5ba7b59c40
Daniel Semeniuta <dsemeniu@stanford.edu>
2019-12-05 16:58:18 -0800

    hyperparam changes

--- run_experiments.py
+++ run_experiments.py
@@ -31,7 +31,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--n_epochs", type=int, default=300, help="number of epochs of training for generator")
     parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
-    parser.add_argument("--lr", type=float, default=0.0004, help="adam: learning rate")
+    parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
     parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
     parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
     parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
