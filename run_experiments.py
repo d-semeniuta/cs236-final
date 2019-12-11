fc421de92f527624ebcae35c80955bcf4fd95181
Daniel Semeniuta <dsemeniu@stanford.edu>
2019-12-10 19:30:01 -0800

    transform changes

--- run_experiments.py
+++ run_experiments.py
@@ -89,7 +89,8 @@ def run_generator(train_data, gen_dir, args):
         print('Images already generated...')
         transform_list = [transforms.Resize(args.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
         if args.channels == 1:
-            transform_list.insert(0, transforms.Grayscale(3))
+            transform_list.append(transforms.Lambda(lambda image: image.convert('RGB')))
+            # transform_list.insert(0, transforms.Grayscale(3))
         gen_data = datasets.ImageFolder(
             out_loc,
             transform=transforms.Compose(transform_list)
@@ -120,7 +121,8 @@ def run_generator(train_data, gen_dir, args):
     util.utils.generateImages(generator, num_images, args, out_loc)
     transform_list = [transforms.Resize(args.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
     if args.channels == 1:
-        transform_list.insert(0, transforms.Grayscale(3))
+        transform_list.append(transforms.Lambda(lambda image: image.convert('RGB')))
+        # transform_list.insert(0, transforms.Grayscale(3))
     gen_data = datasets.ImageFolder(
         out_loc,
         transform=transforms.Compose(transform_list)
